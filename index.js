d81027c71e6fd5eb202735d305df8200e830c643
sterlingwes <swj@wesquire.ca>
2014-05-11 23:33:48 -0300

    fix extend, add bulk register

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var AppDispatch = require('./appdispatcher.js')
-  , extend = require('utils')._extend
+  , extend = function(to,add) {
+      for(var k in add) {
+          to[k] = add[k];
+      }
+      return to;
+  }
   
   , actions = [];
 
 module.exports = {
     
     register: function(name) {
-        if(actions.indexOf(name)==-1)
+        if(typeof name === 'object' && name && name.length)
+            [].push.apply(actions, name);
+        else if(actions.indexOf(name)==-1)
             actions.push(name);
     },
     
